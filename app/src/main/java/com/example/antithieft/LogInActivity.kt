ea159879a768c3f9c2dc85aef779523f1681f078
Lintang Bongkar IRIS <lintang.bb@gmail.com>
2022-01-09 16:27:30 -0800

    Initial commit

---
+++ app/src/main/java/com/example/antithieft/LogInActivity.kt
@@ -0,0 +1,26 @@
+package com.example.antithieft
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_log_in.*
+
+var Uname:String = ""
+var PassW:String = ""
+
+class LogInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_log_in)
+
+        ButLogin.setOnClickListener(){
+            Uname = textUsername.text.toString()
+            PassW = textPassword.text.toString()
+
+
+            Toast.makeText(applicationContext, "wrong Username or Password",Toast.LENGTH_LONG).show()
+
+        }
+    }
+}
